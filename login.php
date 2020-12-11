fac2460496f2db2e45470a26f5ffcbfc6aef47eb
Yann Martin <ymartin@la-providence.net>
2020-12-11 14:15:11 +0100

    Modification valeur nom

--- login.php
+++ login.php
@@ -20,7 +20,7 @@ if(isset($_POST["connexion"])) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo $_INFO['name']; ?> - Connexion</title>
+    <title><?php echo $_INFO['nom']; ?> - Connexion</title>
 
     <link href="//fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
     <link href="//fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300i,700" rel="stylesheet">
